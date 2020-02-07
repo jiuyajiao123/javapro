22aa63a9298e2727cb69589879796dda4098c864
jiuyajiao123 <1325573781@qq.com>
2020-02-07 16:24:34 +0800

    one2

---
+++ src/com/neuedu/text/Text2.java
@@ -0,0 +1,12 @@
+package com.neuedu.text;
+
+/**
+ * Created by 86183 on 2020/2/7.
+ */
+public class Text2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+}
